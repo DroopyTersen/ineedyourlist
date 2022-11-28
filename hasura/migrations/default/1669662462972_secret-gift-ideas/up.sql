b83a3ac9e91d45f96b61db0703c8915fb070b53f
Andrew Petersen <andrew.petersen15@gmail.com>
2022-11-28 14:48:47 -0700

    fixes to allow migration

--- hasura/migrations/default/1669662462972_secret-gift-ideas/up.sql
+++ hasura/migrations/default/1669662462972_secret-gift-ideas/up.sql
@@ -1,8 +1,7 @@
 
 alter table "public"."gift_ideas" alter column "wishlist_id" drop not null;
 
-alter table "public"."gift_ideas" add column "user_id" uuid
- null;
+alter table "public"."gift_ideas" add column "user_id" uuid null;
 
 alter table "public"."gift_ideas"
   add constraint "gift_ideas_user_id_fkey"
